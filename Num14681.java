f0df1927c3b04d298566ba8f868d7f2eca1c8baf
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-03-30 20:22:00 +0900

    baekjoon-Num14681
    https://www.acmicpc.net/problem/14681

---
+++ Num14681.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num14681 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int result = 0;
+		
+		if(x>0 && y>0) {
+			result = 1;
+		}
+		else if(x<0 && y>0) {
+			result = 2;
+		}
+		else if(x<0 && y<0) {
+			result = 3;
+		}
+		else if(x>0 && y<0) {
+			result = 4;
+		}
+		
+		System.out.println(result);
+	}
+}
