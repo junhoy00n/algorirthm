814ef49ce3a2f58282ebb693a01456f46ef440df
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-01 20:03:22 +0900

    baekjoon-Num1550
    https://www.acmicpc.net/problem/1550

---
+++ Num1550.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num1550 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		int num = Integer.parseInt(n, 16);
+		
+		System.out.println(num);
+	}
+}
