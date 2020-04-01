56208f1b3e7c826d5a7d3053bd946b1ce89bce17
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-01 21:25:22 +0900

    Num15727
    https://www.acmicpc.net/problem/15727

---
+++ Num15727.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num15727 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		if(n%5 == 0) {
+			System.out.println(n/5);
+		}
+		else {
+			System.out.println(n/5 +1);
+		}
+	}
+}
