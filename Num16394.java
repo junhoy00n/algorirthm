9ec6cda860579af25d877ce77be85e06819a7a34
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-01 21:53:19 +0900

    baekjoon-Num16394
    https://www.acmicpc.net/problem/16394

---
+++ Num16394.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num16394 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		System.out.println(n-1946);
+	}
+}
