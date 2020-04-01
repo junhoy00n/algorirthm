8b1859f638a7ee4e6c5def8218145a1cbe537c23
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-01 20:46:41 +0900

    baekjoon-Num5522
    https://www.acmicpc.net/problem/5522

---
+++ Num5522.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num5522 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		for(int i=0; i<5; i++) {
+			int num = sc.nextInt();
+			sum += num;
+		}
+		
+		System.out.println(sum);
+	}
+}
