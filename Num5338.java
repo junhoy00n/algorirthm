0e5433a7659978d4e8560e87584e5700c9754132
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-01 20:39:05 +0900

    baekjoon-Num5338
    https://www.acmicpc.net/problem/5338

---
+++ Num5338.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class Num5338 {
+
+	public static void main(String[] args) {
+
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
