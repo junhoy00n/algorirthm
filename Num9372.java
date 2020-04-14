750cb016c7a91ec050a38bb1eb940f5ef9ea20ab
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-04-14 20:55:28 +0900

    baekjoon-Num9372
    https://www.acmicpc.net/problem/9372

---
+++ Num9372.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Num9372 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		byte t = sc.nextByte();
+		for(int i=0; i<t; i++) {
+			short n = sc.nextShort(), m = sc.nextShort();
+			
+			for(short j=0; j<m; j++) {
+				short a = sc.nextShort(), b = sc.nextShort();
+			}
+			
+			System.out.println(n-1);
+		}
+	}
+}
