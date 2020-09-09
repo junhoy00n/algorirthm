ff780e8880f9d4163cd3338b703ab61b0855defe
junhoy00n <59612454+junhoy00n@users.noreply.github.com>
2020-09-09 17:43:38 +0900

    baekjoon_Num10991
    https://www.acmicpc.net/problem/10991

---
+++ baekjoon/Num10991.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+        for(int i=1; i<=n; i++) {
+        	for(int j=i; j<=n-1; j++) {
+        		System.out.print(" ");
+        	}
+        	
+        	for(int j=1; j<=2*i-1; j++) {
+        		if(j%2 == 1) System.out.print("*");
+        		else System.out.print(" ");
+        	}
+        	
+        	System.out.println();
+        }
+	}
+}
